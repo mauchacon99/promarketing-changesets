2770103a0a8c602ed3e460763575a0d7bd873848
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2023-05-10 10:59:24 -0400

    feature

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
+import { add } from "math-helpers";
 import { Button, Header } from "ui";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
+      {add(1, 8)}
       <Button />
     </>
   );
